3ebe3d790f57a958c3b8c637fe445dbd3d8a31ab
Ryan <ryanm@ryans-imac-3.local>
2018-12-07 16:58:34 +1100

    Initial Commit

---
+++ ARKitInteraction/AppDelegate.swift
@@ -0,0 +1,36 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Application's delegate.
+*/
+
+import UIKit
+import ARKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device.
+            """)
+        }
+
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        if let viewController = self.window?.rootViewController as? ViewController {
+            viewController.blurView.isHidden = false
+        }
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let viewController = self.window?.rootViewController as? ViewController {
+            viewController.blurView.isHidden = true
+        }
+    }
+}
